47119673275d9bee3d7b562aeaffb48c48871ceb
Abuwabu <abuwabu@gmail.com>
2016-03-10 15:08:56 +0100

    Add PUT /todos/:id route

--- server.js
+++ server.js
@@ -80,6 +80,36 @@ app.delete('/todos/:id', function (req, res) {
 
 
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10),
+        matchedTodo = _.findWhere(todos, {id: todoId}),
+        body = _.pick(req.body, 'description', 'completed'),
+        validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        // Has property 'completed' but not boolean
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+});
+
+
+
 // Listen up...
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
